490429e06ed6d78af745a748dc616a4ece157a96
ccormon <camille.cormon@protonmail.com>
2024-07-12 14:57:44 +0200

    cpp01

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:41:49 by ccormon           #+#    #+#             */
-/*   Updated: 2024/07/11 17:19:47 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/07/12 14:48:08 by ccormon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ n't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
 
 void	Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve bee\
+	std::cout << "I think I deserve to have some extra bacon for free. I've bee\
 n coming for years whereas you started working here since last month."
 		<< std::endl;
 }
@@ -40,5 +40,17 @@ void	Harl::error(void)
 
 void	Harl::complain(std::string level)
 {
+	std::string	tab[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	bool		option_is_ok = false;
+	void		(Harl::*option[4])(void) = {&Harl::debug, &Harl::info,
+					&Harl::warning, &Harl::error};
 
+	for (int i = 0; i < 4; i++)
+		if (level == tab[i])
+		{
+			(this->*option[i])();
+			option_is_ok = true;
+		}
+	if (!option_is_ok)
+		std::cout << "The option " << level << " doesn't exist." << std::endl;
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:41:23 by ccormon           #+#    #+#             */
-/*   Updated: 2024/07/11 16:42:18 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/07/12 14:44:06 by ccormon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	main(void)
 {
+	Harl	harl;
 
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("HEY");
 }
