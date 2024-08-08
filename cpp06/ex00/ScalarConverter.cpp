43e7b3433eda646f1e7c8274de3a582a6e314e91
ccormon <camille.cormon@gmail.com>
2024-08-08 17:57:34 +0200

    remove function

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 11:22:59 by ccormon           #+#    #+#             */
-/*   Updated: 2024/08/08 17:33:56 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/08/08 17:56:26 by ccormon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,45 +31,34 @@ ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &toCopy)
 	return (*this);
 }
 
-static bool	isGoodFormat(const std::string &toConvert)
+void	ScalarConverter::convert(const std::string &toConvert)
 {
 	if (toConvert.length() == 0)
 	{
 		std::cout << "Error: empty string" << std::endl;
-		return (false);
-	}
-
-	if (toConvert.length() != 1)
-	{
-		for (int i = 0; toConvert[i]; i++)
-		{
-			if (toConvert[i] < 48 || toConvert[i] > 57)
-			{
-				std::cout << "Error: wrong format" << std::endl;
-				return (false);
-			}
-		}
-	}
-	else
-	{
-		if (toConvert[0] < 32 || toConvert[0] > 126)
-		{
-			std::cout << "Error: non displayable character" << std::endl;
-			return (false);
-		}
-	}
-
-	return (true);
-}
-
-void	ScalarConverter::convert(const std::string &toConvert)
-{
-	if (!isGoodFormat(toConvert))
 		return ;
+	}
 
 	char		*end;
 	long double	d = strtold(toConvert.c_str(), &end);
 	long		l = static_cast<long>(d);
+
+	if (*end)
+	{
+		if (toConvert.length() != 1 && )
+		{
+			std::cout << "Error: wrong format" << std::endl;
+			return ;
+		}
+		else
+		{
+			if (*end < 32 || *end > 126)
+			{
+				std::cout << "Error: non displayable character" << std::endl;
+				return ;
+			}
+		}
+	}
 
 	std::cout << "strtold(toConvert.c_str(), &end) = " << d << "\tend = " << end << std::endl;
 	std::cout << "static_cast<long>(d) = " << l << std::endl;
