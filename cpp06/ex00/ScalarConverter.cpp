51732ce7296b46ae1276f8e042b8fad12daeb26d
ccormon <camille.cormon@gmail.com>
2024-08-08 16:10:44 +0200

    check toConvert

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 11:22:59 by ccormon           #+#    #+#             */
-/*   Updated: 2024/08/04 16:38:44 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/08/08 16:05:11 by ccormon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,26 @@ ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &toCopy)
 	return (*this);
 }
 
+static bool	isGoodFormat(const std::string &toConvert)
+{
+
+}
+
 void	ScalarConverter::convert(const std::string &toConvert)
 {
+	if (toConvert.length() == 0)
+		return ;
+
+	if (toConvert.length() != 1)
+	{
+		for (int i = 0; toConvert[i]; i++)
+			if (toConvert[i] < 48 || toConvert[i] > 57)
+				return ;
+	}
+	else
+		if (toConvert[0] < 32 || toConvert[0] > 126)
+			return ;
+
 	char		*end;
 	long double	d = strtold(toConvert.c_str(), &end);
 	long		l = static_cast<long>(d);
